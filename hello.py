61ab349cb1880aaf6871fa37a0f0930c7db86ebe
zhou <zhouyfpg@126.com>
2018-05-24 18:06:37 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,24 @@
+from flask import Flask,request,redirect,render_template
+
+from flask_script import Manager
+
+app=Flask(__name__)
+manager = Manager(app)
+
+
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    #return '<h1>Hello World! %s</h1>'% user_agent
+    return redirect('https://www.google.com')
+
+@app.route('/usr/<name>')
+def user(name):
+    return render_template('usr.html',name=name)
+
+
+
+if __name__ == '__main__':
+    #app.run(debug=True)
+    manager.run()
